7e026335e76d402ce3ca441276c2277d073a1864
Jason Steele <jason.omnibus@gmail.com>
2019-04-20 20:32:00 -0400

    removed weather and eslint

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ const routes = [{
 }]
 
 // add all routes from all modules to the routes array manually or write your routes inside a folder inside the server folder
-// with suffix as Routes.js e.g weatherRoutes.js
+// with suffix as Routes.js e.g userRoutes.js
 glob.sync('./server/**/*Routes.js').forEach((file) => {
   routes.push(require(path.resolve(file)))
 })
